8ff91064c9a796fd6433e924dc4c5a3d91f59fe8
JiminN2 <alie1023@naver.com>
2024-04-26 23:33:38 +0900

    commit

--- src/pages/ItemPage/components/ItemDetailSection.jsx
+++ src/pages/ItemPage/components/ItemDetailSection.jsx
@@ -1,19 +1,12 @@
-import React, { useEffect, useState} from "react";
+import React from "react";
 import ItemCard "./ItemCard";
-import { getProducts } from "../../../api";
 
 
 
 function ItemDetailSection() {
-    const [itemList, setItemList]= useState([]);
-    
     return(
-        <div className="itemDetailContainer">
-            <div className="itemDetailCard">
-                {itemList?.map((item) => (
-                    <ItemCard item={item} key={`item-detail-${item.images}`} />
-                ))}
-            </div>
+        <div>
+            {ItemCard}
         </div>
     )
 }
