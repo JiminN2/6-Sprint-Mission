b43d0b90d39c5b94343a096a348930fc4eb9e7f8
JiminN2 <alie1023@naver.com>
2024-06-07 23:39:30 +0900

    feat: AddArticle

---
+++ styles/CommonStyles.ts
@@ -0,0 +1,9 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  padding: 16px;
+  background-color: pink;
+  margin: auto 360px;
+`;
